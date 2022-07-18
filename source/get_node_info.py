465d5aaab058766c1145a03dafd685e0dcbec0e5
Kai <347036700@qq.com>
2022-07-18 16:56:26 +0800

    add comment

--- source/get_node_info.py
+++ source/get_node_info.py
@@ -3,36 +3,34 @@ import get_ec2_price
 import json
 
 config.load_incluster_config()
-# config.load_kube_config()
 
+
+# get node information
 def get_node_info():
-    try:
-        v1 = client.CoreV1Api()
-        nodes_data = {}
-        nodes = v1.list_node()
-        for node in nodes.items:
-            node_name = node.metadata.name
-            node_instance_type = node.metadata.labels['beta.kubernetes.io/instance-type']
-            node_capacity_type = node.metadata.labels['eks.amazonaws.com/capacityType']
-            node_zone = node.metadata.labels['topology.kubernetes.io/zone']
-            node_region = node.metadata.labels['topology.kubernetes.io/region']
-            node_allocatable_cpu = eval(node.status.allocatable['cpu'][0:-1])
-            node_allocatable_memory = eval(node.status.allocatable['memory'][:-2]) / 1024
-            node_price = get_ec2_price.get_node_price(node_capacity_type, node_region, node_zone, node_instance_type)
-            node_resource = {
-                'node_instance_type': node_instance_type,
-                'node_capacityType': node_capacity_type,
-                'node_zone': node_zone,
-                'node_region': node_region,
-                'node_allocatable_cpu': node_allocatable_cpu,
-                'node_allocatable_memory': node_allocatable_memory,
-                'node_price': node_price
-            }
-            nodes_data[node_name] = node_resource
+    v1 = client.CoreV1Api()
+    nodes_data = {}
+    nodes = v1.list_node()
+    for node in nodes.items:
+        node_name = node.metadata.name
+        node_instance_type = node.metadata.labels['beta.kubernetes.io/instance-type']
+        node_capacity_type = node.metadata.labels['eks.amazonaws.com/capacityType']
+        node_zone = node.metadata.labels['topology.kubernetes.io/zone']
+        node_region = node.metadata.labels['topology.kubernetes.io/region']
+        node_allocatable_cpu = eval(node.status.allocatable['cpu'][0:-1])
+        node_allocatable_memory = eval(node.status.allocatable['memory'][:-2]) / 1024
+        node_price = get_ec2_price.get_node_price(node_capacity_type, node_region, node_zone, node_instance_type)
+        node_resource = {
+            'node_instance_type': node_instance_type,
+            'node_capacityType': node_capacity_type,
+            'node_zone': node_zone,
+            'node_region': node_region,
+            'node_allocatable_cpu': node_allocatable_cpu,
+            'node_allocatable_memory': node_allocatable_memory,
+            'node_price': node_price
+        }
+        nodes_data[node_name] = node_resource
 
-        return nodes_data
-    except Exception as err:
-        print("Error:", err)
+    return nodes_data
 
 
 if __name__ == "__main__":
--- source/get_pod_info.py
+++ source/get_pod_info.py
@@ -5,94 +5,42 @@ from kubernetes.client import api_client
 config.load_incluster_config()
 
 
-# config.load_kube_config()
-
+# list pod information and join metrics
 def get_pod_info():
-    try:
-        v1 = client.CoreV1Api()
-        pods = v1.list_pod_for_all_namespaces(watch=False)
-        pods_data = {}
-        pods_metrics = get_pod_metrics()
-        for pod in pods.items:
-            pod_name = pod.metadata.name
-            pod_namespace = pod.metadata.namespace
-            # measure_name = "%s_%s" % (pod_nsname, pod_name)
-            pod_node_name = pod.spec.node_name
-            if pod_name in pods_metrics:
-                cpu_utilization = pods_metrics[pod_name]['cpu_util']
-                memory_utilization = pods_metrics[pod_name]['memory_util']
-            else:
-                cpu_utilization = 0
-                memory_utilization = 0
-            pod_resource = {
-                "cpu_request": 0,
-                "memory_request": 0,
-                "pod_namespace": pod_namespace,
-                "pod_node_name": pod_node_name,
-                "cpu_utilization": cpu_utilization,
-                "memory_utilization": memory_utilization
-            }
-            for container in pod.spec.containers:
-                requests = container.resources.requests
-                if requests:
-                    cpu = requests.get("cpu", None)
-                    memory = requests.get("memory", None)
-                    if cpu:
-                        if cpu[-1] == "m":
-                            cpu = eval(cpu[0:-1])
-                        else:
-                            cpu = eval(cpu) * 1000
-                        pod_resource["cpu_request"] += cpu
-                    if memory:
-                        memory_mi = eval(memory[0:-2])
-                        if memory[-2:] == "Gi":
-                            memory_mi = memory_mi * 1024
-                        elif memory[-2:] == "Mi":
-                            memory_mi = memory_mi
-                        elif memory[-2:] == "Ki":
-                            memory_mi = memory_mi / 1024
-                        else:
-                            memory_mi = eval(memory) / 1024 / 1024
-                        pod_resource["memory_request"] += memory_mi
-                else:
-                    # print(pod_name, "requests not configured")
-                    break
-            pods_data[pod_name] = pod_resource
-
-        return pods_data
-    except Exception as err:
-        print("Error:", err)
-
-
-def get_pod_metrics():
-    try:
-        # Creating a dynamic client
-        dynamic_client = dynamic.DynamicClient(
-            api_client.ApiClient(configuration=config.load_incluster_config())
-        )
-
-        # fetching the node api
-        api = dynamic_client.resources.get(api_version="metrics.k8s.io/v1beta1", kind="PodMetrics")
-        pod_data = {}
-        for item in api.get().items:
-            pod_name = item.metadata.name
-            pod_utilization = {
-                "cpu_util": 0,
-                "memory_util": 0
-            }
-            for i in item.containers:
-                cpu = i.usage.cpu
-                memory = i.usage.memory
+    v1 = client.CoreV1Api()
+    pods = v1.list_pod_for_all_namespaces(watch=False)
+    pods_data = {}
+    pods_metrics = get_pod_metrics()
+    for pod in pods.items:
+        pod_name = pod.metadata.name
+        pod_namespace = pod.metadata.namespace
+        # measure_name = "%s_%s" % (pod_nsname, pod_name)
+        pod_node_name = pod.spec.node_name
+        if pod_name in pods_metrics:
+            cpu_utilization = pods_metrics[pod_name]['cpu_util']
+            memory_utilization = pods_metrics[pod_name]['memory_util']
+        else:
+            cpu_utilization = 0
+            memory_utilization = 0
+        pod_resource = {
+            "cpu_request": 0,
+            "memory_request": 0,
+            "pod_namespace": pod_namespace,
+            "pod_node_name": pod_node_name,
+            "cpu_utilization": cpu_utilization,
+            "memory_utilization": memory_utilization
+        }
+        for container in pod.spec.containers:
+            requests = container.resources.requests
+            if requests:
+                cpu = requests.get("cpu", None)
+                memory = requests.get("memory", None)
                 if cpu:
                     if cpu[-1] == "m":
                         cpu = eval(cpu[0:-1])
-                    elif cpu[-1] == "n":
-                        cpu = eval(cpu[0:-1]) / 1000 / 1000
-                    elif cpu[-1] == "u":
-                        cpu = eval(cpu[0:-1]) / 1000
                     else:
                         cpu = eval(cpu) * 1000
-                    pod_utilization["cpu_util"] += cpu
+                    pod_resource["cpu_request"] += cpu
                 if memory:
                     memory_mi = eval(memory[0:-2])
                     if memory[-2:] == "Gi":
@@ -103,11 +51,57 @@ def get_pod_metrics():
                         memory_mi = memory_mi / 1024
                     else:
                         memory_mi = eval(memory) / 1024 / 1024
-                    pod_utilization["memory_util"] += memory_mi
-            pod_data[pod_name] = pod_utilization
-        return pod_data
-    except Exception as err:
-        print("Error:", err)
+                    pod_resource["memory_request"] += memory_mi
+            else:
+                # print(pod_name, "requests not configured")
+                break
+        pods_data[pod_name] = pod_resource
+
+    return pods_data
+
+
+# get pod metrics from metrics server
+def get_pod_metrics():
+    # Creating a dynamic client
+    dynamic_client = dynamic.DynamicClient(
+        api_client.ApiClient(configuration=config.load_incluster_config())
+    )
+
+    # fetching the node api
+    api = dynamic_client.resources.get(api_version="metrics.k8s.io/v1beta1", kind="PodMetrics")
+    pod_data = {}
+    for item in api.get().items:
+        pod_name = item.metadata.name
+        pod_utilization = {
+            "cpu_util": 0,
+            "memory_util": 0
+        }
+        for i in item.containers:
+            cpu = i.usage.cpu
+            memory = i.usage.memory
+            if cpu:
+                if cpu[-1] == "m":
+                    cpu = eval(cpu[0:-1])
+                elif cpu[-1] == "n":
+                    cpu = eval(cpu[0:-1]) / 1000 / 1000
+                elif cpu[-1] == "u":
+                    cpu = eval(cpu[0:-1]) / 1000
+                else:
+                    cpu = eval(cpu) * 1000
+                pod_utilization["cpu_util"] += cpu
+            if memory:
+                memory_mi = eval(memory[0:-2])
+                if memory[-2:] == "Gi":
+                    memory_mi = memory_mi * 1024
+                elif memory[-2:] == "Mi":
+                    memory_mi = memory_mi
+                elif memory[-2:] == "Ki":
+                    memory_mi = memory_mi / 1024
+                else:
+                    memory_mi = eval(memory) / 1024 / 1024
+                pod_utilization["memory_util"] += memory_mi
+        pod_data[pod_name] = pod_utilization
+    return pod_data
 
 
 if __name__ == "__main__":
